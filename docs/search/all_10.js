9d1efb5b12b8fc41c08fc3b9c4865bf26e945e4f
Aleksei Dynda <alexey.dynda@gmail.com>
2020-03-07 18:18:24 +1000

    Documentation is updated

--- docs/search/all_10.js
+++ docs/search/all_10.js
@@ -22,7 +22,7 @@ var searchData=
   ['setframerate',['setFrameRate',['../class_nano_engine_core.html#a398aa88d0dac78e9edade0d965896a96',1,'NanoEngineCore']]],
   ['setfreefont',['setFreeFont',['../class_nano_canvas_ops.html#ac19e475c216bed1b313155ea89a0852f',1,'NanoCanvasOps::setFreeFont()'],['../class_nano_display_base.html#a17b8466fc09a1aa758adc7a32ef57958',1,'NanoDisplayBase::setFreeFont()']]],
   ['setmode',['setMode',['../class_nano_canvas_ops.html#aeeac54bfbd5728e64755a5fe27efa1e0',1,'NanoCanvasOps']]],
-  ['setoffset',['setOffset',['../class_adafruit_canvas_ops.html#a2c6e7bba427fbc0e9e3b8e36fcd1b0ab',1,'AdafruitCanvasOps::setOffset()'],['../class_nano_canvas_ops.html#a4bef3a62e787ca2390470ef95a77257e',1,'NanoCanvasOps::setOffset()'],['../class_nano_display_base.html#a182078088dd5caabbd242d75a7bbe48a',1,'NanoDisplayBase::setOffset()']]],
+  ['setoffset',['setOffset',['../class_adafruit_canvas_ops.html#a2c6e7bba427fbc0e9e3b8e36fcd1b0ab',1,'AdafruitCanvasOps::setOffset()'],['../class_nano_canvas_ops.html#a4bef3a62e787ca2390470ef95a77257e',1,'NanoCanvasOps::setOffset()'],['../class_interface_s_t7735.html#a8ffc24c8f82c7c53b1685e8214118903',1,'InterfaceST7735::setOffset()']]],
   ['setpoint',['setPoint',['../struct___nano_point.html#a0cca7797568bd0d06f00fd8fb54695d0',1,'_NanoPoint']]],
   ['setpos',['setPos',['../struct_s_p_r_i_t_e.html#aefc379df23410812d5799c2b93161e19',1,'SPRITE::setPos()'],['../class_nano_object.html#a6ce66d0e05547006224125b31573eb56',1,'NanoObject::setPos()']]],
   ['setrect',['setRect',['../struct___nano_rect.html#a4ec46c67e816c1462b4f69ff874e9e8f',1,'_NanoRect']]],
