dbbe57ec228a105d293477528144e1a1b2ef0bb1
lexus2k <alexey.dynda@gmail.com>
2021-11-30 14:42:42 +1000

    Fixing CI for ESP32

--- .travis/setup_esp_build_env.sh
+++ .travis/setup_esp_build_env.sh
@@ -7,7 +7,9 @@ else
     # sudo apt-get install libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
     #
     mkdir -p ~/esp
-    git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    # Switching to 4.3 version, since v5.0-dev doesn't support Makefiles anymore
+    git clone -b v4.3 --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    # git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
 fi
 
 cd ~/esp/esp-idf
